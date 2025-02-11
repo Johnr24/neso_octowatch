c26e0c63a0ac01d17a0e9e6caea5f061eb264da8
John Rogers <johnrogers24@gmail.com>
2025-02-11 13:05:31 +0000

    go go go

--- custom_components/neso_octowatch/sensor.py
+++ custom_components/neso_octowatch/sensor.py
@@ -1,8 +1,13 @@
 """Platform for sensor integration."""
 from __future__ import annotations
 
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.components.sensor import SensorDeviceClass
+from datetime import datetime
+import zoneinfo
+from homeassistant.components.sensor import (
+    SensorEntity,
+    SensorDeviceClass,
+    SensorStateClass,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -54,26 +59,32 @@ class NesoOctowatchSensor(CoordinatorEntity, SensorEntity):
         self._sensor_type = sensor_type
         self._attr_name = SENSOR_NAMES[sensor_type]
         self._attr_unique_id = f"{DOMAIN}_{sensor_type}"
+        self._attr_native_unit_of_measurement = None
+        self._attr_device_class = None
+        self._attr_state_class = None
 
         # Set appropriate device class and units based on sensor type
         if sensor_type == SENSOR_UTILIZATION:
-            self._attr_native_unit_of_measurement = "%"
-            self._attr_device_class = None  # Remove device class since it can be text or numeric
+            # Will be set dynamically based on value type
+            pass
         elif sensor_type == SENSOR_DELIVERY_DATE:
             self._attr_device_class = SensorDeviceClass.TIMESTAMP
         elif sensor_type == SENSOR_TIME_WINDOW:
-            self._attr_device_class = None  # Text value
+            pass  # Text value, no special handling needed
         elif sensor_type == SENSOR_PRICE:
             self._attr_native_unit_of_measurement = "GBP/MWh"
             self._attr_device_class = SensorDeviceClass.MONETARY
+            self._attr_state_class = SensorStateClass.MEASUREMENT
             self._attr_suggested_display_precision = 2
         elif sensor_type == SENSOR_VOLUME:
             self._attr_native_unit_of_measurement = "MW"
             self._attr_device_class = SensorDeviceClass.POWER
+            self._attr_state_class = SensorStateClass.MEASUREMENT
             self._attr_suggested_display_precision = 1
         elif sensor_type == SENSOR_HIGHEST_ACCEPTED:
             self._attr_native_unit_of_measurement = "GBP/MWh"
             self._attr_device_class = SensorDeviceClass.MONETARY
+            self._attr_state_class = SensorStateClass.MEASUREMENT
             self._attr_suggested_display_precision = 2
 
     @callback
@@ -88,16 +99,36 @@ class NesoOctowatchSensor(CoordinatorEntity, SensorEntity):
                 sensor_data = self.coordinator.data[key]
                 state_value = sensor_data.get("state")
                 
-                # Handle utilization value specifically
-                if self._sensor_type == SENSOR_UTILIZATION:
-                    if isinstance(state_value, (int, float)):
-                        self._attr_native_value = float(state_value)
-                        self._attr_native_unit_of_measurement = "%"
+                try:
+                    if self._sensor_type == SENSOR_UTILIZATION:
+                        # For utilization, only set percentage unit if it's a number
+                        if isinstance(state_value, (int, float)) or (
+                            isinstance(state_value, str) and 
+                            state_value.replace('.', '', 1).isdigit()
+                        ):
+                            self._attr_native_value = float(state_value)
+                            self._attr_native_unit_of_measurement = "%"
+                            self._attr_state_class = SensorStateClass.MEASUREMENT
+                        else:
+                            self._attr_native_value = state_value
+                            self._attr_native_unit_of_measurement = None
+                            self._attr_state_class = None
+                    elif self._sensor_type == SENSOR_DELIVERY_DATE:
+                        if isinstance(state_value, str):
+                            try:
+                                dt = datetime.strptime(state_value, "%Y-%m-%d")
+                                # Set to noon UTC to avoid timezone issues
+                                dt = dt.replace(hour=12, minute=0, second=0, microsecond=0, tzinfo=zoneinfo.ZoneInfo("UTC"))
+                                self._attr_native_value = dt
+                            except ValueError:
+                                self._attr_native_value = None
+                        else:
+                            self._attr_native_value = state_value
                     else:
                         self._attr_native_value = state_value
-                        self._attr_native_unit_of_measurement = None
-                else:
+                except (ValueError, TypeError):
                     self._attr_native_value = state_value
+                
                 self._attr_extra_state_attributes = sensor_data.get("attributes", {})
             else:
                 self._attr_native_value = None
